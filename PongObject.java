6012c97c37ce95c65a11db288ab64e8c2aa67f58
sshingane <samir.shingane@gmail.com>
2017-01-05 23:31:25 -0800

    Abstract class
    This file is the abstract class upon which the other files are built
    upon.

---
+++ PongObject.java
@@ -0,0 +1,19 @@
+import acm.graphics.GObject;
+
+
+public abstract class PongObject extends GObject{
+
+	public PongObject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public abstract GObject get();
+	
+	public abstract double getX();
+	public abstract double getY();
+}
+
+
+
+
+
