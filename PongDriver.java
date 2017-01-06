599143b4611664e4f6d49ba0e0dd36bcd52046cc
sshingane <samir.shingane@gmail.com>
2017-01-05 23:30:27 -0800

    Starter file
    This file is the starter file of the game.

---
+++ PongDriver.java
@@ -0,0 +1,15 @@
+/**
+ * Driver for Pong
+ * @author QuadraPong
+ *
+ */
+
+public class PongDriver {
+
+	public static void main(String[] args) {
+		Pong pong = new Pong();
+		// pong.init(); // invoke the applet's init() method
+		pong.start(); // starts the applet
+
+	}
+}
